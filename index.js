2b386160cc00468b876049870b679044c54935c1
rahulbabu7 <rahulbabu436@gmail.com>
2023-10-18 07:19:06 +0530

    add exam

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// let str1 = "rahul";
+// let reversedStr = str1.split('').reverse().join('');
+// alert(reversedStr);
+// console.log(reversedStr);
+
+// // let str1 = "rahul";
+// let rev = "";
+
+// for (let i = str1.length - 1; i >= 0; i--) {
+//     rev += str1[i];
+// }
+
+// alert(rev);
+
+
+document.writeln("<table border=1>");
+document.writeln("<tr> <th> N</th><th> N</th><th> N</th> </tr>")
+
+for (let i = 1; i < 10; i++) {
+    document.writeln("<tr><td>" + i + "</td><td>" + i * 10 + "</td><td>" + i * 100 + "</td></tr>");
+}
+
+document.writeln("</table>");
+
